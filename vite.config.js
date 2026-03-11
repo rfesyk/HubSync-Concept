eae935613fe081362f5e3742215012e89383ebdb
Rostyslav <rostyslavfesyk@gmail.com>
2026-03-11 20:13:49 +0100

    Allow Railway preview host

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,7 @@ export default defineConfig({
       '9982-2a02-a311-a7-9000-1196-9bee-1d32-960a.ngrok-free.app',
     ],
   },
+  preview: {
+    allowedHosts: ['hubsync-concept-production-cf38.up.railway.app'],
+  },
 })
